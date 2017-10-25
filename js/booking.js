3f2eef37552ce69cc906a356250783552dee0cfa
Unknown <up703451@myport.ac.uk>
2017-10-25 14:46:16 +0100

    Added calender and file download button
    - The calender doesnt stay open so might have to change how this page looks

---
+++ js/booking.js
@@ -0,0 +1,13 @@
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 1, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false,
+    // Close upon selecting a date,
+    disable: [
+        1,4,7
+    ]
+
+});
