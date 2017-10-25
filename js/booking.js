3f35186b173729a4866ef22def8367064ce3a7cf
Unknown <up703451@myport.ac.uk>
2017-10-25 19:10:52 +0100

    Added a modal to click off the book button
    Full calander isnt working with google maps

--- js/booking.js
+++ js/booking.js
@@ -1,12 +1,25 @@
+(function ($) {
+    $(function () {
+
+        //initialize all modals
+        $('.modal').modal();
+
+        //now you can open modal from code
+        $('#modal1').modal('open');
+
+        //or by click on trigger
+        $('.trigger-modal').modal();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
 
 
 $(document).ready(function() {
-
-    // page is now ready, initialize the calendar...
-
     $('#calendar').fullCalendar({
-        // put your options and callbacks here
-        weekends: false // will hide Saturdays and Sundays
-    })
+        events: {
+            googleCalendarApiKey: 'AIzaSyC1-aVybRt6Ld1A23kHEe3H0pKsgxEgbao',
+            googleCalendarId: 'jhpocdlbno13rep1kntd151gt0%40group.calendar.google.com',
+        }
+    });
 
 });
