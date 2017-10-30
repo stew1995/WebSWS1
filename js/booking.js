2b6d299347f29b2cfd5b733128895f96cda57e07
Unknown <up703451@myport.ac.uk>
2017-10-30 13:18:17 +0000

    Added class timetable summary to booking
    - Now need to make changes on the timetable below, add the disablled days ect

--- js/booking.js
+++ js/booking.js
@@ -1,25 +1,11 @@
-(function ($) {
-    $(function () {
-
-        //initialize all modals
-        $('.modal').modal();
-
-        //now you can open modal from code
-        $('#modal1').modal('open');
-
-        //or by click on trigger
-        $('.trigger-modal').modal();
-
-    }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-
 $(document).ready(function() {
     $('#calendar').fullCalendar({
         events: {
             googleCalendarApiKey: 'AIzaSyC1-aVybRt6Ld1A23kHEe3H0pKsgxEgbao',
             googleCalendarId: 'jhpocdlbno13rep1kntd151gt0%40group.calendar.google.com',
         }
+
+        //Need to add the disabled weeks
     });
 
 });
