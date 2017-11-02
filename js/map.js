608a634af0d9a0463fff2e114387593fa2f5e11b
Unknown <up703451@myport.ac.uk>
2017-11-02 12:29:48 +0000

    Google Map in

--- js/map.js
+++ js/map.js
@@ -19,3 +19,12 @@ navigator.geolocation.getCurrentPosition(locationNow)//, options);
 
 
 
+
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
